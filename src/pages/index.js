504ea8260252ae4d416d1cf6a18bfb232677b853
krishnasaga <sagar474@hotmail.com>
2021-03-06 13:55:49 +0000

    add all

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Box } from 'theme-ui';
+
+
+const Explorer = () => {
+  return <Box sx={{
+    boxShadow: ' box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);'
+  }} >
+
+  </Box>
+};
 
 export default () => <div>
-  <a href={"/companies-by-sector"}>Explore Companies by Sector</a>
+  <Explorer>
+    <a href={"/companies-by-sector"}>Explore Companies by Sector</a>
+  </Explorer>
 </div>;
