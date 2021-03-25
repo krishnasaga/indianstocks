76f04e4a4d8807fdd780b47d756137c2f1b3c0c6
krishnasaga <sagar474@hotmail.com>
2021-03-25 22:20:08 +0000

    add all

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
-import {Box, Grid, Text,Link} from 'theme-ui';
+import {Box, Grid, Link, Text} from 'theme-ui';
 import {Portal} from "react-portal";
 import OutsideClickHandler from 'react-outside-click-handler';
+import {useSpring,animated} from 'react-spring';
 
-
+const AnimatedBox = animated(Box);
 
 export const TopNav = () => {
-  const [open,setOpen] = useState(false);
-  const [dropdownComponent,setDropdownComponent] = useState('');
+  const [open, setOpen] = useState(false);
+  const [dropdownComponent, setDropdownComponent] = useState('');
 
-  const menuHandler = (event,componentName) => {
+  const menuHandler = (event, componentName) => {
     setOpen(!open);
     setDropdownComponent(componentName)
     event.stopPropagation();
@@ -20,28 +21,28 @@ export const TopNav = () => {
     top: '0',
     zIndex: '99',
     width: '90%'
-  }} >
-    <Grid columns={['1fr 1fr 1fr 1fr 1fr 1fr 1fr']} >
+  }}>
+    <Grid columns={['1fr 1fr 1fr 1fr 1fr 1fr 1fr']}>
       <Box p={25} sx={{
         gridAlignSelf: 'start'
-      }}> <Link href={'/'} sx={{ textDecoration: 'none', color: 'black'}}>Invest </Link></Box>
-      <Box p={25} >  </Box>
-      <Menu onClick={(event) => menuHandler(event,'disruptions')}> Disruptions </Menu>
-      <Menu onClick={(event) => menuHandler(event,'research')} > Research </Menu>
-      <Menu onClick={(event) => menuHandler(event,'sectors')} > Sectors </Menu>
-      <Menu onClick={(event) => menuHandler(event,'analytics')} > Analytics </Menu>
+      }}> <Link href={'/'} sx={{textDecoration: 'none', color: 'black'}}>Invest </Link></Box>
+      <Box p={25}> </Box>
+      <Menu onClick={(event) => menuHandler(event, 'disruptions')}> Disruptions </Menu>
+      <Menu onClick={(event) => menuHandler(event, 'research')}> Research </Menu>
+      <Menu onClick={(event) => menuHandler(event, 'sectors')}> Sectors </Menu>
+      <Menu onClick={(event) => menuHandler(event, 'analytics')}> Analytics </Menu>
       <NavButton/>
     </Grid>
-    { open ? <Dropdown component={dropdownComponent}/> : null }
-    <OutsideClickHandler useCapture={true} onOutsideClick={()=>{
-      setOpen(false );
+    {open ? <Dropdown component={dropdownComponent}/> : null}
+    <OutsideClickHandler useCapture={true} onOutsideClick={() => {
+      setOpen(false);
     }}/>
   </Box>;
 };
 
 
-function NavButton({children,...remainingProps}){
-  return  <Box {...remainingProps} p={25}  color={'color2'} bg={'primary'} sx={{
+function NavButton({children, ...remainingProps}) {
+  return <Box {...remainingProps} p={25} color={'color2'} bg={'primary'} sx={{
     width: '100%',
     textAlign: 'center',
     gridAlignSelf: 'end',
@@ -53,8 +54,8 @@ function NavButton({children,...remainingProps}){
   </Box>;
 }
 
-export const Menu = ({children,...remainingProps}) => {
-  return  <Box {...remainingProps} >
+export const Menu = ({children, ...remainingProps}) => {
+  return <Box {...remainingProps} >
     <Box
       p={20} color={'color5'} sx={{
       textAlign: 'center',
@@ -75,91 +76,98 @@ const dropdownComponents = {
   sectors: SectorsDropdown
 };
 
-function Dropdown({component}){
+function Dropdown({component}) {
   const Component = dropdownComponents[component];
+
+  const props = useSpring({opacity: 1, from: {opacity: 0}})
+
   return <Portal>
-    <Box onClick={(event)=>{
+    <AnimatedBox onClick={(event) => {
       event.stopPropagation();
     }
-    } p={20} bg={'color4'} color={'color2'} sx={{
+    } p={20} bg={'color4'} color={'color2'}
+      sx={{
       position: 'fixed',
       left: '0',
       top: '74px',
       width: '90%',
-      boxShadow: '4px 4px 10px -5px rgb(0 0 0 / 10%)'
-    }}>
+      boxShadow: '4px 4px 10px -5px rgb(0 0 0 / 10%)',
+
+    }}
+    style={props}
+    >
       <Component/>
-    </Box></Portal>;
+    </AnimatedBox></Portal>;
 }
 
-function AnalyticsDropdown(){
-  return <Box  sx={{
+function AnalyticsDropdown() {
+  return <Box sx={{
     height: '200px'
-  }} >
+  }}>
     <Text py={10}>Analytics</Text>
     <Grid columns={[4]}>
-      <Box sx={{ borderRight: '1px solid grey'}}>
-        <Text color={'color2'} > Electric two wheeler penetration </Text>
-        <Text color={'color2'} > Electric Cars penetration </Text>
-        <Text color={'color2'} > Battery ecosystems </Text>
-        <Text color={'color2'} > Materials </Text>
+      <Box sx={{borderRight: '1px solid grey'}}>
+        <Text color={'color2'}> Electric two wheeler penetration </Text>
+        <Text color={'color2'}> Electric Cars penetration </Text>
+        <Text color={'color2'}> Battery ecosystems </Text>
+        <Text color={'color2'}> Materials </Text>
       </Box>
       <Box>
-        <Text color={'color2'} >  Telecom performance </Text>
-        <Text color={'color2'} >  5G ready </Text>
+        <Text color={'color2'}> Telecom performance </Text>
+        <Text color={'color2'}> 5G ready </Text>
       </Box>
       <Box>
-        <Text color={'color2'} >  Information Technology </Text>
-        <Text color={'color2'} >  Internet of things </Text>
+        <Text color={'color2'}> Information Technology </Text>
+        <Text color={'color2'}> Internet of things </Text>
       </Box>
       <Box>
-        <Text color={'color2'} >  Bioinformatics usage </Text>
-        <Text color={'color2'} >  Telemedicine penetration </Text>
+        <Text color={'color2'}> Bioinformatics usage </Text>
+        <Text color={'color2'}> Telemedicine penetration </Text>
       </Box>
     </Grid>
   </Box>
 }
 
-function DisruptionsDropdown(){
-  return <Box  sx={{
+function DisruptionsDropdown() {
+  return <Box sx={{
     height: '200px'
-  }} >
+  }}>
     <Text py={20}>Disruptions</Text>
     <Grid columns={[4]}>
-      <Box sx={{ borderRight: '1px solid grey'}}>
-        <Text color={'color2'} >  Electric Transport </Text>
-        <Text color={'color2'} >  Genomics </Text>
-        <Text color={'color2'} >  Battery ecosystems </Text>
-        <Text color={'color2'} >  Materials </Text>
+      <Box sx={{borderRight: '1px solid grey'}}>
+        <Text color={'color2'}> Electric Transport </Text>
+        <Text color={'color2'}> Genomics </Text>
+        <Text color={'color2'}> Battery ecosystems </Text>
+        <Text color={'color2'}> Materials </Text>
       </Box>
       <Box>
-        <Text color={'color2'} >  Space Exploration </Text>
-        <Text color={'color2'} >  Next IT </Text>
+        <Text color={'color2'}> Space Exploration </Text>
+        <Text color={'color2'}> Next IT </Text>
       </Box>
       <Box>
-        <Text color={'color2'} >  Information Technology </Text>
-        <Text color={'color2'} >  Internet of things </Text>
+        <Text color={'color2'}> Information Technology </Text>
+        <Text color={'color2'}> Internet of things </Text>
       </Box>
       <Box>
-        <Text color={'color2'} >  Bioinformatics  </Text>
-        <Text color={'color2'} >  Telemedicine  </Text>
+        <Text color={'color2'}> Bioinformatics </Text>
+        <Text color={'color2'}> Telemedicine </Text>
       </Box>
     </Grid>
   </Box>
 }
 
-function ResearchDropdown(){
-  return <Box  sx={{
+function ResearchDropdown() {
+  return <Box sx={{
     height: '200px'
-  }} >
+  }}>
     Research
   </Box>
 }
 
-function SectorsDropdown(){
+function SectorsDropdown() {
   return <Box sx={{
     height: '200px'
-  }} >
+  }}>
     Sectors
   </Box>
 }
