12472fd377f59f1d640a958f101406dfa9e282e1
hari <hsri0007@gmail.com>
2021-03-26 18:51:02 +0530

    added url

--- src/somethings/sectors.js
+++ src/somethings/sectors.js
@@ -1,88 +1,93 @@
 module.exports = [
   {
-    name: 'healthcare',
-    displayName: 'Health Care',
-    intro: 'Healthcare industry in India is projected to reach $372 billion by 2022',
-    backgroundImage: 'healthcare-background.jpg',
+    name: "healthcare",
+    displayName: "Health Care",
+    intro:
+      "Healthcare industry in India is projected to reach $372 billion by 2022",
+    backgroundImage: "health-insurance.png",
     focus: [
       {
-        name: 'DNA Sequencing'
+        name: "DNA Sequencing",
       },
       {
-        name: 'Bioinformatics'
+        name: "Bioinformatics",
       },
       {
-        name: 'Generating'
+        name: "Generating",
       },
       {
-        name: 'Pharmaceutical Products'
+        name: "Pharmaceutical Products",
       },
-
     ],
     insights: [
       {
-        name: 'Startups',
-        number: 25
+        name: "Startups",
+        number: 25,
       },
       {
-        name: 'Listed companies',
-        number: 25
+        name: "Listed companies",
+        number: 25,
       },
       {
-        name: 'Private Companies',
-        number: 25
+        name: "Private Companies",
+        number: 25,
       },
       {
-        name: 'CAGR',
-        number: '25%'
-      }
-    ]
+        name: "CAGR",
+        number: "25%",
+      },
+    ],
   },
   {
-    name: 'energy',
-    displayName: 'Energy',
+    name: "energy",
+    displayName: "Energy",
+    backgroundImage: "health-insurance.png",
     focus: [
       {
-        name: 'Renewable Energy'
+        name: "Renewable Energy",
       },
       {
-        name: 'Energy Storage'
-      }
-    ]
+        name: "Energy Storage",
+      },
+    ],
   },
   {
-    name: 'aerospace',
-    displayName: 'Aerospace',
+    name: "aerospace",
+    displayName: "Aerospace",
+    backgroundImage: "health-insurance.png",
     focus: [
       {
-        name: 'Satellite Networks'
+        name: "Satellite Networks",
       },
       {
-        name: 'Orbital Launch'
+        name: "Orbital Launch",
       },
       {
-        name: 'Space Travel'
+        name: "Space Travel",
       },
       {
-        name: 'Pharmaceutical Products'
-      }
-    ]
+        name: "Pharmaceutical Products",
+      },
+    ],
   },
   {
-    name: 'transportation',
-    displayName: 'Transportation',
+    name: "transportation",
+    displayName: "Transportation",
+    backgroundImage: "health-insurance.png",
   },
   {
-    name: 'telecommunications',
-    displayName: 'Telecommunications',
+    name: "telecommunications",
+    displayName: "Telecommunications",
+    backgroundImage: "health-insurance.png",
   },
   {
-    name: 'information-technology',
-    displayName: 'Information Technology',
-
+    name: "information-technology",
+    displayName: "Information Technology",
+    backgroundImage: "health-insurance.png",
   },
   {
-    name: 'materials',
-    displayName: 'Materials',
-  }
+    name: "materials",
+    displayName: "Materials",
+    backgroundImage: "health-insurance.png",
+  },
 ];
