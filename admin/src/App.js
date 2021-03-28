79db6a2c1a46aaa7873e812907b6877964481118
krishnasaga <sagar474@hotmail.com>
2021-03-28 11:45:36 +0100

    add changes to admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Admin, Resource } from 'react-admin';
 import { restClient } from 'ra-data-feathers';
-import { List, Datagrid, TextField, DateField, BooleanField,Create,SimpleForm,TextInput } from 'react-admin';
+import { List, Datagrid, TextField, DateField, BooleanField,Create,SimpleForm,TextInput,ArrayInput,SimpleFormIterator,Edit } from 'react-admin';
 import fetch from 'isomorphic-fetch';
 
 const rest = require('@feathersjs/rest-client');
@@ -20,7 +20,7 @@ function App() {
     <Admin className="App" dataProvider={restClient(app, {
       id: '_id', // If your database uses an id field other than 'id'. Optional.
     })}>
-      <Resource name={'sector'} list={SectorsList} create={SectorsCreate}/>
+      <Resource name={'sector'} list={SectorsList} create={SectorsCreate} edit={SectorsEdit}/>
     </Admin>
   );
 }
@@ -29,8 +29,10 @@ function App() {
 function SectorsList(props){
   return <List {...props}>
     <Datagrid>
+      <TextField source="_id" />
       <TextField source="name" />
       <TextField source="displayName" />
+      <TextField source="intro" />
     </Datagrid>
   </List>;
 }
@@ -41,7 +43,34 @@ export const SectorsCreate = (props) => (
       <TextInput source="name" />
       <TextInput source="displayName" />
       <TextInput source="intro" />
+      <TextInput source="intro" />
+      <ArrayInput source="insights">
+        <SimpleFormIterator>
+          <TextInput source="naem" />
+          <TextInput source="number" />
+        </SimpleFormIterator>
+      </ArrayInput>
     </SimpleForm>
   </Create>
 );
+
+
+export const SectorsEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>      <TextInput source="_id" />
+
+      <TextInput source="name" />
+      <TextInput source="displayName" />
+      <TextInput source="intro" />
+      <TextInput source="intro" />
+      <ArrayInput source="insights">
+        <SimpleFormIterator>
+          <TextInput source="naem" />
+          <TextInput source="number" />
+        </SimpleFormIterator>
+      </ArrayInput>
+    </SimpleForm>
+  </Edit>
+);
+
 export default App;
