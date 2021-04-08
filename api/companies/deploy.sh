34e297ccb892432f79ff0e439808b8a922d65b75
krishnasaga <sagar474@hotmail.com>
2021-04-09 00:34:26 +0100

    add force redirects to server

--- api/companies/deploy.sh
+++ api/companies/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 zip -r lambda.zip *
 aws lambda update-function-code \
-    --function-name  indianstocks-sectors \
+    --function-name  indianstocks-companies \
     --zip-file fileb://lambda.zip
