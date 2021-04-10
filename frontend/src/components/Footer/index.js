c1b572ed5e768976c5700ec57c2194cbe50b83b9
krishnasaga <sagar474@hotmail.com>
2021-04-10 12:42:21 +0100

    add some changes to forntend

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { Box } from 'theme-ui';
+import {Box,Grid} from 'theme-ui';
 
-export default () => <Box>
- This is footer
-</Box>;
+export const Footer = () => {
+  return <Box
+    as={'footer'}
+    bg={'color4'}
+    sx={{
+      height: '400px'
+    }}>
+    <Box>
+      <Grid
+      columns={['1fr 1fr 1fr 1fr']}
+      alignItems={'center'}>
+      <Box>
+        Hello
+      </Box>
+      <Box>
+        Hello
+      </Box>
+      <Box>
+        Hello
+      </Box>
+      <Box>
+        Hello
+      </Box>
+    </Grid>
+    </Box>
+  </Box>;
+};
