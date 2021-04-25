f0ab5e1883d7964a06552b4221fb309a47c86c9d
NiStoj <nikolina.utkovska@gmail.com>
2021-04-25 13:29:10 +0200

    add cards

--- frontend/src/components/Cards/index.js
+++ frontend/src/components/Cards/index.js
@@ -1,45 +1,68 @@
 import React from 'react';
-import {Box, Grid, Text, Card} from 'theme-ui';
+import { Box, Grid, Text } from 'theme-ui';
+import { BsFillGearFill } from 'react-icons/bs'
+
+const CardWrapper = ({ children }) => {
+    return (
+        <Box
+            ml={10} pb={50} sx={{
+            maxWidth: 300,
+            height: 400,
+            border: '1px solid rgb(255, 255, 255)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            color: 'color4',
+            textAlign: 'center',
+        }}
+        >
+            {children}
+        </Box>
+    );
+};
+
+const CardText = ({ children }) => {
+    return (
+        <Text p={20} sx={{
+            color: 'white',
+            fontWeight: 'bold',
+            paddingX: '60px'
+        }}
+        >
+            {children}
+        </Text>
+    );
+};
 
 export const Cards = () => {
     return <Box
         as={'cards'}
-        bg={'color4'}
         sx={{
-            color: 'rgb(255, 255, 255)',
-            fontSize: '0.9rem'
+            fontSize: '0.9rem',
+            paddingBottom: '50px',
         }}>
-        <Box>
+        <Box >
             <Grid
-                columns={[2, 4]}>
-                <Card
-                    sx={{
-                        maxWidth: 256,
-                    }}>
-                    <Image src={images.nyc} />
-                    <Text>Card text</Text>
-                </Card>
-                <Card
-                    sx={{
-                        maxWidth: 256,
-                    }}>
-                    <Image src={images.nyc} />
-                    <Text>Card text</Text>
-                </Card>
-                <Card
-                    sx={{
-                        maxWidth: 256,
-                    }}>
-                    <Image src={images.nyc} />
-                    <Text>Card text</Text>
-                </Card>
-                <Card
-                    sx={{
-                        maxWidth: 256,
-                    }}>
-                    <Image src={images.nyc} />
-                    <Text>Card text</Text>
-                </Card>
+                columns={[1, 2, 4]} sx={{
+                // justifyItems: 'center',
+            }}>
+            <CardWrapper>
+              <BsFillGearFill size={100} />
+              <CardText>ARK Disruptive Innovation</CardText>
+            </CardWrapper>
+            <CardWrapper>
+              <BsFillGearFill size={100} />
+              <CardText>Autonomous Tech & Robotics</CardText>
+            </CardWrapper>
+            <CardWrapper>
+              <BsFillGearFill size={100} />
+              <CardText>Genomic Revolution</CardText>
+            </CardWrapper>
+            <CardWrapper>
+              <BsFillGearFill size={100} />
+              <CardText>Next Generation Internet</CardText>
+            </CardWrapper>
             </Grid>
         </Box>
     </Box>;
