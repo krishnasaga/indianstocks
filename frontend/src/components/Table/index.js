d577ab9144ee1bee099ff90b246a28c24996ef65
NiStoj <nikolina.utkovska@gmail.com>
2021-06-04 18:58:33 +0200

    add StickyColCell comp to table

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -1,10 +1,220 @@
-import React from 'react'
-import { Box } from 'theme-ui';
+import React from 'react';
+import {Box, Text, Image, Grid} from 'theme-ui';
+import './index.css';
 
+const StickyColCell = ({children}) => {
+  return (
+      <Grid sx={{
+        gridAutoFlow: "column",
+      }}>
+        <Image src={'/company-icons/tata.png'} width={'40px'} height={'40px'} sx={{alignSelf: 'center'}}/>
+        <Text sx={{
+          color: "red",
+          fontSize: "1rem",
+        }}>
+          {children}
+        </Text>
+      </Grid>
+  )
+}
 
 export const Table = () => {
   return <Box>
     Table
+
+    <div role="region" aria-labelledby="caption" tabIndex="0">
+      <table>
+        <caption id="caption">Sticky table</caption>
+        <thead>
+        <tr>
+          <th>Company Name</th>
+          <th>Focus</th>
+          <th>PE ratio</th>
+          <th>PB ratio</th>
+          <th>Market Cap</th>
+          <th>Revenue</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <StickyColCell>Name</StickyColCell>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <StickyColCell>Name</StickyColCell>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        <tr>
+          <th>Name</th>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+          <td>Hello</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </Box>;
 }
 
