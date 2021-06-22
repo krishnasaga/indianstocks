867f5545620936d454d5f3ab60eb7e46b6b33a64
krishnasaga <sagar474@hotmail.com>
2021-06-22 21:34:43 +0100

    add rows in the table

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -1,108 +1,108 @@
-import React, { useState } from 'react';
-import { Box, Grid, Image, Text, Button } from 'theme-ui';
+import React, {useState} from 'react';
+import {Box, Button, Grid, Image, Text} from 'theme-ui';
 import './index.css';
-import { AiOutlineStar } from 'react-icons/ai';
+import {AiOutlineStar} from 'react-icons/ai';
 
-function ToolTip ({ children, ...remainingProps }) {
+function ToolTip({children, ...remainingProps}) {
   const [isShown, setIsShown] = useState(false);
 
   return (
-      <Box className="App">
-        <Button sx={{
-          color: "lightgrey",
-          padding: '0',
-          position: "relative",
-          zIndex: "0",
-          background: "none",
-          ':hover': {
-            color: "orange",
-            cursor: "pointer",
-          }
-        }}
-            onMouseEnter={() => setIsShown(true)}
-            onMouseLeave={() => setIsShown(false)}>
-          <AiOutlineStar size={16}/>
-        </Button >
-        {isShown && (
-            <Text sx={{
-              clipPath: "polygon(0 0, 100% 0%, 100% 75%, 55% 75%, 50% 100%, 45% 75%, 0% 75%)",
-              color: "white",
-              background: "black",
-              position: 'absolute',
-              zIndex: "10",
-              fontSize: "0.8rem",
-              padding: "5px 5px 15px 5px",
-              borderRadius: "6px",
-            }}>
-              Add to Main Watchlist
-            </Text>
-        )}
-      </Box>
+    <Box className="App">
+      <Button sx={{
+        color: "lightgrey",
+        padding: '0',
+        position: "relative",
+        zIndex: "0",
+        background: "none",
+        ':hover': {
+          color: "orange",
+          cursor: "pointer",
+        }
+      }}
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}>
+        <AiOutlineStar size={16}/>
+      </Button>
+      {isShown && (
+        <Text sx={{
+          clipPath: "polygon(0 0, 100% 0%, 100% 75%, 55% 75%, 50% 100%, 45% 75%, 0% 75%)",
+          color: "white",
+          background: "black",
+          position: 'absolute',
+          zIndex: "10",
+          fontSize: "0.8rem",
+          padding: "5px 5px 15px 5px",
+          borderRadius: "6px",
+        }}>
+          Add to Main Watchlist
+        </Text>
+      )}
+    </Box>
   );
 }
 
 const StickyColStar = () => {
   return (
-      <Box as={'td'} sx={{position: 'sticky', left: 0}}>
-        <Grid sx={{
-          gridAutoFlow: "column",
-          alignItems: "center",
-          justifyContent: "start",
-        }}>
-          {/*<Button sx={{*/}
-          {/*  color: "lightgrey",*/}
-          {/*  padding: '0',*/}
-          {/*  ':hover': {*/}
-          {/*    color: "orange",*/}
-          {/*    cursor: "pointer",*/}
-          {/*  }*/}
-          {/*}}>*/}
-          {/*  <AiOutlineStar size={18}/>*/}
-          {/*</Button>*/}
+    <Box as={'td'} sx={{position: 'sticky', left: 0}}>
+      <Grid sx={{
+        gridAutoFlow: "column",
+        alignItems: "center",
+        justifyContent: "start",
+      }}>
+        {/*<Button sx={{*/}
+        {/*  color: "lightgrey",*/}
+        {/*  padding: '0',*/}
+        {/*  ':hover': {*/}
+        {/*    color: "orange",*/}
+        {/*    cursor: "pointer",*/}
+        {/*  }*/}
+        {/*}}>*/}
+        {/*  <AiOutlineStar size={18}/>*/}
+        {/*</Button>*/}
         <ToolTip></ToolTip>
-        </Grid>
-      </Box>
+      </Grid>
+    </Box>
   )
 }
 
 const StickyColNumber = ({children}) => {
   return (
-      <Box as={'td'} sx={{position: 'sticky', left: "30px"}}>
-        <Grid sx={{
-          gridAutoFlow: "column",
-          alignItems: "center",
-          justifyContent: "start",
+    <Box as={'td'} sx={{position: 'sticky', left: "30px"}}>
+      <Grid sx={{
+        gridAutoFlow: "column",
+        alignItems: "center",
+        justifyContent: "start",
+      }}>
+        <Text sx={{
+          color: "grey",
+          fontSize: "0.8rem",
         }}>
-          <Text sx={{
-            color: "grey",
-            fontSize: "0.8rem",
-          }}>
-            {children}
-          </Text>
-        </Grid>
-      </Box>
+          {children}
+        </Text>
+      </Grid>
+    </Box>
   )
 }
 
 const StickyColName = ({children}) => {
   return (
-      <Box as={'td'} sx={{position: 'sticky', left: "57px"}}>
-        <Grid sx={{
-          gridAutoFlow: "column",
-          alignItems: "center",
-          justifyContent: "start",
+    <Box as={'td'} sx={{position: 'sticky', left: "57px"}}>
+      <Grid sx={{
+        gridAutoFlow: "column",
+        alignItems: "center",
+        justifyContent: "start",
 
+      }}>
+        <Image src={'/company-icons/tata.png'} width={'40px'} height={'40px'} sx={{alignSelf: 'center'}}/>
+        <Text sx={{
+          color: "#222531FF",
+          fontSize: "0.95rem",
+          fontWeight: "600"
         }}>
-          <Image src={'/company-icons/tata.png'} width={'40px'} height={'40px'} sx={{alignSelf: 'center'}}/>
-          <Text sx={{
-            color: "#222531FF",
-            fontSize: "0.95rem",
-            fontWeight: "600"
-          }}>
-            {children}
-          </Text>
-        </Grid>
-      </Box>
+          {children}
+        </Text>
+      </Grid>
+    </Box>
   )
 }
 
@@ -115,11 +115,10 @@ export const Table = () => {
           <th></th>
           <th>#</th>
           <th>Company Name</th>
-          <th>Focus</th>
-          <th>PE ratio</th>
-          <th>PB ratio</th>
           <th>Market Cap</th>
           <th>Revenue</th>
+          <th>PE ratio</th>
+          <th>PB ratio</th>
         </tr>
         </thead>
         <tbody>
@@ -134,229 +133,21 @@ export const Table = () => {
         {/*  <td>Hello</td>*/}
         {/*  <td>Hello</td>*/}
         {/*</tr>*/}
-        <tr>
-          <StickyColStar>
 
-          </StickyColStar>
-          <StickyColNumber>1</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td><ToolTip></ToolTip></td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>2</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>3</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
+        {
+          new Array(100).fill(1).map((value, index) => {
+            return <tr key={index}>
+              <StickyColStar>Star</StickyColStar>
+              <StickyColNumber>6</StickyColNumber>
+              <StickyColName>Name</StickyColName>
+              <Box as={'td'} color={'#000000'}>$32,569.21</Box>
+              <Box as={'td'} color={'#000000'}>$32,569.21</Box>
+              <Box as={'td'} color={'green'}>15.5</Box>
+              <Box as={'td'} color={'red'}>12</Box>
+            </tr>;
+          })
+        }
 
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>4</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>5</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>6</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>7</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>8</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>9</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>10</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>11</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>12</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>13</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>14</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>15</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>16</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>17</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>18</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>19</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>20</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>21</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <StickyColStar>Star</StickyColStar>
-          <StickyColNumber>22</StickyColNumber>
-          <StickyColName>Name</StickyColName>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-          <td>Hello</td>
-        </tr>
         </tbody>
       </table>
     </div>
