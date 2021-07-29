257ab6c46d5711da00a1b812fb4dd26420ff8395
krishnasaga <sagar474@hotmail.com>
2021-07-29 21:35:17 +0100

    remove gatsby google analytics plugin

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -38,12 +38,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
